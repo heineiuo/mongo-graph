74a8b7edb920449b2400e61fbb5bac1eec0a950e
heineiuo <heineiuo@gmail.com>
2017-07-03 16:27:14 +0800

    add schema

--- src/Triple.js
+++ src/Triple.js
@@ -1,3 +1,14 @@
+import uuid from 'uuid'
+import {ObjectId} from 'mongodb'
+
+const schema = {
+  subject: {type: String, default: uuid.v1},
+  object: {type: String, default: uuid.v1}, // 可以是dbref (当描述树的关系的时候)
+  predicate: {type: String}, 
+  createTime: {type: Date, default: Date.now},
+}
+
+
 class Triple {
 
 }
--- src/Vertex.js
+++ src/Vertex.js
@@ -1,4 +1,22 @@
+import uuid from 'uuid'
+import {ObjectId} from 'mongodb'
+
+const getDefaultSubjectProps = () => {
+  return {
+
+  }
+}
+
+const schema = {
+  type: {type: String, default: 'people'},
+  props: {type: Object, default: getDefaultSubjectProps},
+  createTime: {type: Date, default: Date.now},
+  updateTime: {type: Date, default: Date.now},
+}
+
 class Vertex {
+
+
   constructor(db){
     this.db = db
   }
